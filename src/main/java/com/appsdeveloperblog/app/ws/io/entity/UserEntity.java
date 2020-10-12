89a7e5bd1d8bfec84d75f720de60803e3ada9159
Parvez Nawaz <ParvezN@gs1ca.org>
2020-10-12 08:24:59 -0400

    JPQL

--- src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
@@ -4,7 +4,8 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
-@Entity(name = "users")
+@Entity
+@Table(name="users")
 public class UserEntity implements Serializable {
     private static final long serialVersionUID = 5313493413859894403L;
     @Id
